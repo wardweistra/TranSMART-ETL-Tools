9c5fe83d6f88101044ceb472c4f247dcd8432ec5
Ward Weistra <ward@thehyve.nl>
2014-01-16 16:02:13 +0100

    Added command line options instead of hard coded platform and organism name

--- createPlatformDefinitionFromGEOSOFTTable.py
+++ createPlatformDefinitionFromGEOSOFTTable.py
@@ -1,8 +1,21 @@
-gpl_id = 'GPL6244'
-organism = 'Homo Sapiens'
+import argparse
+from os.path import join
 
-infile = open('GPL6244.annot','r')
-outfile = open(gpl_id+'-platform.txt', 'w');
+parser = argparse.ArgumentParser(description='Convert the SOFT table file (GPLxxxx.annot) from GEO to the platform file expected by tranSMART.')
+parser.add_argument('platform', default='platform', help='The name of the platform.')
+parser.add_argument('-organism', default='Homo Sapiens', help='The name of the organism.')
+parser.add_argument('-infile', default='[platform].annot', help='The file to be used as an input. Will use [platform].annot if left emtpy.')
+parser.add_argument('-folder', default='./', help='The folder for the in and output files.')
+arguments = parser.parse_args()
+
+gpl_id = arguments.platform
+organism = arguments.organism
+
+if (arguments.infile == '[platform].annot') :
+	arguments.infile = arguments.platform+'.annot'
+
+infile = open(join(arguments.folder,arguments.infile),'r')
+outfile = open(join(arguments.folder,gpl_id+'-platform.txt'), 'w');
 
 #firstlinewords = infile.readline().strip('\n').split('\t')
 outfile.write('GPL_ID\tPROBE_ID\tGENE_SYMBOL\tGENE_ID\tORGANISM')
